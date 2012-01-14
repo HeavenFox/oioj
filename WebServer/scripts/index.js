90cf09f2a2ef936e6e0588b24268179af232ba55
Zhu Jingsi <heavenfox@heavenfox.org>
2012-01-15 00:32:25 +0800

    fixed the bug that bullets do not enlarge when selected

--- WebServer/scripts/index.js
+++ WebServer/scripts/index.js
@@ -12,10 +12,38 @@ function showCAPTCHA()
 	}
 }
 
-var currentBannerBullet;
+var curBanner = 1;
+var totalBanner;
+var bannerTimeout;
 
 function moveBannerTo(n)
 {
 	$("#bn_banners").css("top",-(n-1)*360+"px");
-	//currentBannerBullet = $('')
+	$("#bn_links a img").css("width","10px").css("height","10px").css("margin","2px");
+	$("#bn_links a:nth-child("+n+")").children().css("width","14px").css("height","14px").css("margin","0px");
 }
+
+function autoMoveBanner()
+{
+	curBanner++;
+	if (curBanner > totalBanner)curBanner = 1;
+	moveBannerTo(curBanner);
+	setAutoBanner();
+}
+
+function setAutoBanner()
+{
+	bannerTimeout = setTimeout('autoMoveBanner()', 10000);
+}
+
+function stopAutoBanner()
+{
+	clearTimeout(bannerTimeout);
+}
+
+$(function(){
+	totalBanner = $("#bn_links a").size();
+	moveBannerTo(curBanner);
+	setAutoBanner();
+});
+
