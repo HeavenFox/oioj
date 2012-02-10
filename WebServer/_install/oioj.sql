ee37cd5234ef15d8ef011f5fc73383bce179a724
Zhu Jingsi <heavenfox@heavenfox.org>
2012-02-10 18:34:32 +0800

    added comment schema to main sql

--- WebServer/_install/oioj.sql
+++ WebServer/_install/oioj.sql
@@ -189,6 +189,16 @@ CREATE TABLE `oj_problem_attachments` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `oj_problem_comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent` int(11) NOT NULL DEFAULT '0',
+  `uid` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `timestamp` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
 CREATE TABLE `oj_problem_tags` (
   `pid` int(11) NOT NULL,
   `tid` int(11) NOT NULL,
