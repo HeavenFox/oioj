851b033701728c87917b74d951a208bfe1d08f62
Zhu Jingsi <heavenfox@heavenfox.org>
2012-02-11 15:56:31 +0800

    fixed a bug in submission script that prevent record from refreshing

--- WebServer/scripts/submitsoln/submitsoln_prob.js
+++ WebServer/scripts/submitsoln/submitsoln_prob.js
@@ -20,20 +20,22 @@ function checkRecord(id)
 		newParams = jQuery.extend({
 			url: 'index.php?mod=submit&solution=1&ajax=1',
 			handler: function(data){
-			setTimeout('checkRecord('+data.result.record_id+')',3000);
-			$.fancybox("<div id='submit_infobox'><p>Congratulations. Your program has been submitted successfully.</p><p>Record Information</p><p>ID: "+data.result.record_id+"<br />Server:"+data.result.server_name+"</p><p>We will automatically display update information here, or, if you prefer, click <a href='#' onclick='popupRecord("+data.result.record_id+");return false;'>here</a> to display a popup window.</p></div>",
-			{
-				'autoDimensions'	: false,
-				'width'     : 350,
-				'height'    : 250,
-				'scrolling'	: 'yes',
-				'onClosed'  : function()
+				fancyclosed = false;
+				setTimeout('checkRecord('+data.result.record_id+')',3000);
+				$.fancybox("<div id='submit_infobox'><p>Congratulations. Your program has been submitted successfully.</p><p>Record Information</p><p>ID: "+data.result.record_id+"<br />Server:"+data.result.server_name+"</p><p>We will automatically display update information here, or, if you prefer, click <a href='#' onclick='popupRecord("+data.result.record_id+");return false;'>here</a> to display a popup window.</p></div>",
 				{
-					fancyclosed = true;
-				}
-			});
+					'autoDimensions'	: false,
+					'width'     : 350,
+					'height'    : 250,
+					'scrolling'	: 'yes',
+					'onClosed'  : function()
+					{
+						fancyclosed = true;
+					}
+				});
 				
-			}},params);
+			}
+		},params);
 		this.submitsoln(newParams);
 		return this;
 	}
